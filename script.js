90707136a4ca5dd166a7ddae9b938a6930c3367e
Daniel Rendon <rendod99@gmail.com>
2020-08-19 13:35:42 -0500

    added current day,date, month and year to display

---
+++ script.js
@@ -0,0 +1,38 @@
+var currentDayEl = $('#currentDay')
+var date = new Date();{
+
+var today = date.getDate()
+
+var weekday = new Array(7);
+weekday[0] = "Sunday";
+weekday[1] = "Monday";
+weekday[2] = "Tuesday";
+weekday[3] = "Wednesday";
+weekday[4] = "Thursday";
+weekday[5] = "Friday";
+weekday[6] = "Saturday";
+var dayEl = weekday[date.getDay()];
+
+var month = new Array(12);
+month[0] = "January";
+month[1] = "February";
+month[2] = "March";
+month[3] = "April";
+month[4] = "May";
+month[5] = "June";
+month[6] = "July";
+month[7] = "August";
+month[8] = "September";
+month[9] = "October";
+month[10] = "November";
+month[11] = "December";
+var monthEl = month[date.getMonth() + 1];
+
+var currentYear = date.getFullYear();
+
+$('#currentDay').append(dayEl + ", " +monthEl +' ' + today + ', ' + currentYear);
+}
+
+
+
+
