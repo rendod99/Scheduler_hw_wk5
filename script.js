91d75b4a3d1638efb01d58a7c4f230aeba460b8e
Daniel Rendon <rendod99@gmail.com>
2020-08-20 13:50:38 -0500

    added a table and added css properties to time blocks

--- script.js
+++ script.js
@@ -1,40 +1,15 @@
-var currentDayEl = $('#currentDay')
-var m = moment();{
+//var container = $('.container')
+//var currentDayEl = $('#currentDay')
+var m = moment();
 var toString = (m.toString())
 
-
-var today = m.date()
-
-var weekday = new Array(7);
-weekday[0] = "Sunday";
-weekday[1] = "Monday";
-weekday[2] = "Tuesday";
-weekday[3] = "Wednesday";
-weekday[4] = "Thursday";
-weekday[5] = "Friday";
-weekday[6] = "Saturday";
-var dayEl = weekday[m.day()];
-
-var month = new Array(12);
-month[0] = "January";
-month[1] = "February";
-month[2] = "March";
-month[3] = "April";
-month[4] = "May";
-month[5] = "June";
-month[6] = "July";
-month[7] = "August";
-month[8] = "September";
-month[9] = "October";
-month[10] = "November";
-month[11] = "December";
-var monthEl = month[m.month() + 1];
-
-var currentYear = m.year();
-
 $('#currentDay').append(m.format("dddd MMM Mo YYYY"));
-}
 
 
+$(".table").addClass("time-block");
+$(".row").addClass("row");
+$(".hour").addClass("hour"); 
+$(".container").addClass("description");
+$(".save").addClass("saveBtn");
 
 
